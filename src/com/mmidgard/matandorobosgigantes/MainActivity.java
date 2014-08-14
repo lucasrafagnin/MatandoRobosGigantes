e0c8884f7d9fecf528d4f84263571a219cdcc9b7
Lucas Rafagnin <lucas_rafagnin@hotmail.com>
2014-08-14 16:56:05 -0300

    funcionando o feed do mrg

--- src/com/mmidgard/matandorobosgigantes/MainActivity.java
+++ src/com/mmidgard/matandorobosgigantes/MainActivity.java
@@ -1,8 +1,20 @@
 package com.mmidgard.matandorobosgigantes;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import nl.matshofman.saxrssreader.RssFeed;
+import nl.matshofman.saxrssreader.RssItem;
+import nl.matshofman.saxrssreader.RssReader;
+
+import org.xml.sax.SAXException;
+
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -23,18 +35,39 @@ public class MainActivity extends Activity {
 		// } catch (Exception e) {
 		// e.printStackTrace();
 		// }
-		
-		
+
+		// new AsyncTask<Void, Void, Void>() {
+		// @Override
+		// protected Void doInBackground(Void... params) {
+		// new ReaderRSS().percorrerFeed();
+		// return null;
+		// }
+		// }.execute();
+
 		new AsyncTask<Void, Void, Void>() {
 			@Override
 			protected Void doInBackground(Void... params) {
-				new ReaderRSS().percorrerFeed();
+				try {
+					URL url = new URL("http://jovemnerd.com.br/categoria/matando-robos-gigantes/feed/");
+					RssFeed feed = RssReader.read(url);
+
+					ArrayList<RssItem> rssItems = feed.getRssItems();
+					for (RssItem rssItem : rssItems) {
+						Log.i("RSS Reader", rssItem.getTitle());
+						Log.i("Link: ", rssItem.getLink());
+					}
+
+				} catch (MalformedURLException e) {
+					e.printStackTrace();
+				} catch (SAXException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 				return null;
 			}
 		}.execute();
-		
-	}
 
-	
+	}
 
 }
