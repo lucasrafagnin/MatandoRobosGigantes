e50d76bee5e4b5204ebf39d36231128cd1dcb84e
Lucas Rafagnin <lucas_rafagnin@hotmail.com>
2014-08-15 16:04:26 -0300

    Adicionado links do twitter na pagina inicial

--- src/com/mmidgard/matandorobosgigantes/activity/MainActivity.java
+++ src/com/mmidgard/matandorobosgigantes/activity/MainActivity.java
@@ -5,12 +5,14 @@ import java.util.List;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.mmidgard.matandorobosgigantes.EpisodioFeedParser;
 import com.mmidgard.matandorobosgigantes.R;
@@ -24,6 +26,10 @@ public class MainActivity extends Activity {
 	private Button show;
 	private ProgressDialog dialog;
 
+	private ImageButton beto;
+	private ImageButton diogo;
+	private ImageButton affonso;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -33,6 +39,9 @@ public class MainActivity extends Activity {
 		podcast = (Button)findViewById(R.id.menu_ouvir);
 		vinhetas = (Button)findViewById(R.id.menu_vinhetas);
 		show = (Button)findViewById(R.id.menu_mrgshow);
+		beto = (ImageButton)findViewById(R.id.beto);
+		diogo = (ImageButton)findViewById(R.id.diogo);
+		affonso = (ImageButton)findViewById(R.id.affonso);
 
 		dialog = new ProgressDialog(this);
 
@@ -103,6 +112,54 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 
+			}
+		});
+
+		beto.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = null;
+				try {
+					getPackageManager().getPackageInfo("com.twitter.android", 0);
+					intent = new Intent(Intent.ACTION_VIEW, Uri.parse("twitter://user?user_id=56361939"));
+					intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				} catch (Exception e) {
+					intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/56361939"));
+				}
+				startActivity(intent);
+			}
+		});
+
+		diogo.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = null;
+				try {
+					getPackageManager().getPackageInfo("com.twitter.android", 0);
+					intent = new Intent(Intent.ACTION_VIEW, Uri.parse("twitter://user?user_id=76767920"));
+					intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				} catch (Exception e) {
+					intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/76767920"));
+				}
+				startActivity(intent);
+			}
+		});
+
+		affonso.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = null;
+				try {
+					getPackageManager().getPackageInfo("com.twitter.android", 0);
+					intent = new Intent(Intent.ACTION_VIEW, Uri.parse("twitter://user?user_id=77225484"));
+					intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				} catch (Exception e) {
+					intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/77225484"));
+				}
+				startActivity(intent);
 			}
 		});
 	}
